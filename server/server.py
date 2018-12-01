56a45e80822f9cd8f0aee0954787f848a01347de
Shak <shak582@hotmail.com>
2018-12-01 17:39:08 -0500

    complete db and add some more stuff

--- server/server.py
+++ server/server.py
@@ -3,25 +3,31 @@ from flask import jsonify
 from flask import request
 from flask import session
 from flask.ext.session import Session
-from user import db
-from user import User
+from user import *
 
 
 app = Flask(__name__)
 app.config['SESSION_TYPE'] = 'filesystem'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 Session(app)
 
-
+#USER STUFF##########################################
 @app.route('/login', methods=['POST'])
 def login():
 	req_data = request.get_json()
 	if 'username' in req_data and 'password' in req_data:
-		u = User.query.filter_by(username=req_data['username']).first()
-		if u.password == str(hash(req_data['password'])):
-			session['username'] = req_data['username']
-			print(session['username'])
-			return jsonify({'success' : 'right shit'})
-	return jsonify({'error' : 'wrong request'})
+		try:
+			u = User.query.filter_by(username=req_data['username']).first()
+			if u.password == str(hash(req_data['password'])):
+				session['username'] = req_data['username']
+				print(session['username'])
+				#return jsonify({'success' : 'right shit'})
+				return 'success'
+		except Exception:
+			return 'error'
+	#return jsonify({'error' : 'wrong request'})
+	return 'error'
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -33,15 +39,55 @@ def register():
 			u.password = hash(req_data['password'])
 			db.session.add(u)
 			db.session.commit()
-			return jsonify({'success' : 'right shit'})
+			#return jsonify({'success' : 'right shit'})
+			return 'success'
 		except Exception:
-			return jsonify({'error' : 'user already exists'})
-	return jsonify({'error' : 'wrong request'})
+			#return jsonify({'error' : 'user already exists'})
+			return 'success'
+	#return jsonify({'error' : 'wrong request'})
+	return 'error'
 
 
 @app.route('/getusername', methods=['GET'])
 def getusername():
 	return session['username']
+
+#MATCH STUFF################################################
+@app.route('/creatematch', methods=['POST'])
+def createMatch():
+	req_data = request.get_json()
+	if 'username' in session and 'match' in req_data:
+		try:
+			m = Match()
+			m.match = req_data['match']
+			m.player1 = session['username']
+			m.state = 'QB1'
+			db.session.add(m)
+			db.session.commit()
+			return jsonify({'success' : 'right shit'})
+		except Exception as e:
+			return jsonify({'error':e.args})
+	return jsonify({'error' : 'wrong request or not logged in'})
+
+@app.route('/joinmatch', methods=['POST'])
+def joinMatch():
+	req_data = request.get_json()
+	if 'username' in session and 'match' in req_data:
+		try:
+			m = Match.query.filter_by(match=req_data['match']).first()
+			if not m:
+				return jsonify({'error' : 'match doesnt exist'})
+			if m.player1 == session['username']:
+				return jsonify({'error' : 'user is already in match'})
+			m.player2 = session['username']
+			db.session.commit()
+		except Exception as e:
+			return jsonify({'error' : 'match dont exist'})
+	return jsonify({'sucess' : 'good shit'})
+
+#@app.route('/')
+
+
 
 
 app.run()
--- server/user.py
+++ server/user.py
@@ -9,3 +9,21 @@ class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(80), unique=True, nullable=False)
 	password = db.Column(db.String(256), nullable=False)
+
+
+class Match(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	match = db.Column(db.String(80), unique = True, nullable = False)
+	player1 = db.Column(db.String(80), db.ForeignKey('user.username'), nullable = False)
+	player2 = db.Column(db.String(80), db.ForeignKey('user.username'), nullable=True)
+	state = db.Column(db.String(80), nullable=False)
+
+class Team(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	match = db.Column(db.String(80), db.ForeignKey('match.match'), nullable=False)
+	player = db.Column(db.String(80), db.ForeignKey('user.username'), nullable=False)
+	QB = db.Column(db.String(80), unique=False, nullable=True)
+	RB = db.Column(db.String(80), unique=False, nullable=True)
+	WR = db.Column(db.String(80), unique=False, nullable=True)
+	K = db.Column(db.String(80), unique=False, nullable=True)
+	TE = db.Column(db.String(80), unique=False, nullable=True)
