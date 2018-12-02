d4773d0977ecfd3b30ca76ddbb7979b564142311
Shak <shak582@hotmail.com>
2018-12-02 17:42:52 -0500

    started doing stats and draft

--- server/server.py
+++ server/server.py
@@ -4,6 +4,7 @@ from flask import request
 from flask import session
 from flask.ext.session import Session
 from user import *
+import nflgame
 
 
 app = Flask(__name__)
@@ -11,6 +12,39 @@ app.config['SESSION_TYPE'] = 'filesystem'
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 #app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 Session(app)
+
+games = nflgame.games(2017, week=[x for x in range(1, 18)])
+players = nflgame.combine_game_stats(games)
+
+passing = [x for x in players.passing()]
+players = nflgame.combine_game_stats(games)
+rushing = [x for x in players.rushing()]
+players = nflgame.combine_game_stats(games)
+receiver = [x for x in players.receiving()]
+players = nflgame.combine_game_stats(games)
+kicking = [x for x in players.kicking()]
+
+# for x in passing:
+# 	print x, ' ',
+
+# print '################'
+
+rushing = set(rushing) - set(passing)
+
+# for x in rushing:
+# 	print x, ' ',
+
+# print '###################'
+
+# for x in receiver:
+# 	print x, ' ',
+
+# print '######################'
+
+# for x in kicking:
+# 	print x, ' ',
+
+# print '#####################'
 
 #USER STUFF##########################################
 @app.route('/login', methods=['POST'])
@@ -34,6 +68,8 @@ def register():
 	req_data = request.get_json()
 	if 'username' in req_data and 'password' in req_data:
 		try:
+			if User.query.filter_by(username=req_data['username']).first() != None:
+				return 'error'
 			u = User()
 			u.username = req_data['username']
 			u.password = hash(req_data['password'])
@@ -43,7 +79,7 @@ def register():
 			return 'success'
 		except Exception:
 			#return jsonify({'error' : 'user already exists'})
-			return 'success'
+			return 'error'
 	#return jsonify({'error' : 'wrong request'})
 	return 'error'
 
@@ -56,6 +92,7 @@ def getusername():
 @app.route('/creatematch', methods=['POST'])
 def createMatch():
 	req_data = request.get_json()
+	print req_data
 	if 'username' in session and 'match' in req_data:
 		try:
 			m = Match()
@@ -85,7 +122,11 @@ def joinMatch():
 			return jsonify({'error' : 'match dont exist'})
 	return jsonify({'sucess' : 'good shit'})
 
-#@app.route('/')
+@app.route('/draftplayer', methods=['POST'])
+def draftplayer():
+	req_data = request.get_json()
+	if 'match' in req_data and 'player' in req_data:
+		try:
 
 
 
