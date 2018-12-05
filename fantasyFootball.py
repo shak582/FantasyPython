d0085a5a9685fc246abae753346e0101cedb50ab
Gustavo Valery <gustavoevalery@gmail.com>
2018-12-04 21:05:03 -0500

    Update fantasyFootball.py

--- fantasyFootball.py
+++ fantasyFootball.py
@@ -399,19 +399,17 @@ class DraftWidget(QtWidgets.QWidget):  # DraftWidget for drafting
                 self.add_players()
                 self.itemClicked.connect(self.player_click)
 
-            def add_players(self):  # Here we check position and add player to list by last name
-                '''self.player_text_list = s.get(url= 'http://162.243.35.210:5000/getallmatches')
-                for player_text in self.player_text_list.text.split('\n'):
+            def add_players(self):  # Show list of players containing searched last name 
+                '''self.lastName_list = s.get(url= 'list of players being returned from API with this last name')
+                for player_text in self.lastName_list.text.split('\n'):
                     player = QListWidgetItem(player_text)
                     self.addItem(player)'''
 
             def player_click(self, match):
-                # Will add a player depending on if other team has him or not
-                # If your team is full, it will notify you that you can complete draft
-                # And set text to good
-                '''self.joinMatchDict = {'match' : (str(match.text()))}
+                # Will add a player to their team's database checking their and other teams database for that player's name
+                '''self.playerDict = {'player' : (str(player.text()))}
                 headers = {'Content-type' : 'application/json'}
-                r = s.post(url = 'http://162.243.35.210:5000/joinmatch', headers=headers, data=json.dumps(self.joinMatchDict))'''
+                r = s.post(url = 'url to add player to player's team database', headers=headers, data=json.dumps(self.playerDict))'''
 
         # self.player_list = PlayerList(self)
         # self.player_list.show()
